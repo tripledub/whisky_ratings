7fba05112fcfef13f54d867a0751a1616eb48000
Stewart Campbell <stewartcuk@gmail.com>
2020-04-04 15:41:23 +0100

    index action returns JSON hash of whiskeys

--- app/controllers/api/v1/whiskeys_controller.rb
+++ app/controllers/api/v1/whiskeys_controller.rb
@@ -1,5 +1,15 @@
 class Api::V1::WhiskeysController < ApplicationController
   def index
-    head :ok
+    render json: whiskeys
+  end
+
+  private
+
+  def whiskeys
+    @whiskeys ||= whiskey_scope.to_a
+  end
+
+  def whiskey_scope
+    Whiskey.all
   end
 end
--- spec/requests/api/v1/whiskeys_spec.rb
+++ spec/requests/api/v1/whiskeys_spec.rb
@@ -1,9 +1,29 @@
 require 'rails_helper'
 
 describe 'listing whiskeys' do
+  let!(:whiskey_one) do
+    create(:whiskey, title: 'Bowmore 46 year old (distilled 1964), 42.9%')
+  end
+
+  let!(:whiskey_two) do
+    create(:whiskey, title: 'Glenfarclas Family Casks 1954 Cask #1260, 47.2%')
+  end
+
   before { get '/api/v1/whiskeys' }
 
   it 'returns a status code of 200' do
     expect(response).to have_http_status(:success)
+  end
+
+  describe 'returned results' do
+    subject { JSON.parse(response.body) }
+
+    it 'contains whiskey_one' do
+      expect(subject.first['title']).to eq(whiskey_one.title)
+    end
+
+    it 'contains whiskey_two' do
+      expect(subject.last['title']).to eq(whiskey_two.title)
+    end
   end
 end
