fe837003b030264c15fcc86097a62c460872e4a1
Stewart Campbell <stewartcuk@gmail.com>
2020-04-06 14:44:44 +0100

    add some scopes to Whiskey to make searching easier

--- app/models/whiskey.rb
+++ app/models/whiskey.rb
@@ -1,3 +1,7 @@
 class Whiskey < ApplicationRecord
   validates :title, presence: true
+
+  scope :minimum_color, ->(color) { where('color >= ?', color) }
+  scope :minimum_taste, ->(taste) { where('taste >= ?', taste) }
+  scope :minimum_smokiness, ->(smokiness) { where('smokiness >= ?', smokiness) }
 end
--- spec/models/whiskey_spec.rb
+++ spec/models/whiskey_spec.rb
@@ -9,4 +9,34 @@ RSpec.describe Whiskey, type: :model do
       expect(whiskey.valid?).to eq(false)
     end
   end
+
+  describe 'scopes' do
+    let(:tasty_whiskey) { create(:whiskey, title: 'Tasty', taste: 5) }
+    let(:smoky_whiskey) { create(:whiskey, title: 'Smoky', smokiness: 5) }
+    let(:colorful_whiskey) { create(:whiskey, title: 'Colorful', color: 5) }
+
+    describe '.minimum_color' do
+      subject { Whiskey.all.minimum_color(3) }
+
+      it { is_expected.to include(colorful_whiskey) }
+      it { is_expected.to_not include(smoky_whiskey) }
+      it { is_expected.to_not include(tasty_whiskey) }
+    end
+
+    describe '.minimum_taste' do
+      subject { Whiskey.all.minimum_taste(3) }
+
+      it { is_expected.to_not include(colorful_whiskey) }
+      it { is_expected.to_not include(smoky_whiskey) }
+      it { is_expected.to include(tasty_whiskey) }
+    end
+
+    describe '.minimum_color' do
+      subject { Whiskey.all.minimum_smokiness(3) }
+
+      it { is_expected.to_not include(colorful_whiskey) }
+      it { is_expected.to include(smoky_whiskey) }
+      it { is_expected.to_not include(tasty_whiskey) }
+    end
+  end
 end
