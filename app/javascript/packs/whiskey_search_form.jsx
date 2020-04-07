a6ab26ef2e33564b807aaee039572415cf81457c
Stewart Campbell <stewartcuk@gmail.com>
2020-04-07 14:04:59 +0100

    can now search on ratings (needs styles)

--- app/javascript/packs/whiskey_search_form.jsx
+++ app/javascript/packs/whiskey_search_form.jsx
@@ -6,12 +6,19 @@ import qs from 'qs';
 class WhiskeySearchForm extends React.Component {
   performSearch() {
     let query = ReactDOM.findDOMNode(this.refs.query).value;
+    let taste = ReactDOM.findDOMNode(this.refs.taste).value;
+    let color = ReactDOM.findDOMNode(this.refs.color).value;
+    let smokiness = ReactDOM.findDOMNode(this.refs.smokiness).value;
+
     let self = this;
 
     let search_params = qs.stringify(
       {
         search: {
-          query: query
+          query: query,
+          taste: taste,
+          color: color,
+          smokiness: smokiness
         }
       }
     );
@@ -33,13 +40,39 @@ class WhiskeySearchForm extends React.Component {
   render() {
 
     return(
-      <input
-        onChange={this.performSearch.bind(this)}
-        type="text"
-        className="form-control"
-        placeholder="Search ..."
-        ref="query"
-      /> 
+      <div>
+        <input
+          onChange={this.performSearch.bind(this)}
+          type="text"
+          className="form-control"
+          placeholder="Search ..."
+          ref="query"
+        />
+        <select onChange={this.performSearch.bind(this)} ref="taste" className="form-control">
+          <option value="0">0</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+        <select onChange={this.performSearch.bind(this)} ref="color" className="form-control">
+          <option value="0">0</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+        <select onChange={this.performSearch.bind(this)} ref="smokiness" className="form-control">
+          <option value="0">0</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+      </div>
     )
   }
 }
