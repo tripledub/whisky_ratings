19ce67626afc7bccb70b060312fb6cea312a4f2b
Stewart Campbell <stewartcuk@gmail.com>
2020-04-07 12:56:44 +0100

    add some random ratings to the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
 require 'csv'
 
-
+ratings = (1..5).to_a
 CSV.parse(File.read("#{File.dirname(__FILE__)}/seeds/whiskeys.csv"), headers: true).each do |row|
-  Whiskey.create({ title: row['name'], description: row['description'] })
+  Whiskey.create(
+    {
+      title: row['name'],
+      description: row['description'],
+      taste: ratings.sample,
+      color: ratings.sample,
+      smokiness: ratings.sample
+    }
+  )
 end
 
